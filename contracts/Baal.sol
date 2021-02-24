84c35a95f32ddeb4911c524cb03fd485ded85138
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-02-24 16:52:36 -0500

    Update Baal.sol
    add governance flexibility // gas optimizations

--- contracts/Baal.sol
+++ contracts/Baal.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 /// @dev interface for Baal extensions
 interface MemberAction {
     function memberBurn(address member, uint256 votes) external; // vote-weighted member burn - e.g., "ragequit" to claim capital
-    function memberMint(address member, uint256 amount) external; // amount-weighted member vote mint - e.g., submit direct "tribute" for votes
+    function memberMint(address member) external payable returns (uint256); // amount-weighted member vote mint - e.g., submit direct "tribute" for votes
 }
 
 /// @dev contains modifier for reentrancy check
@@ -25,11 +25,11 @@ contract ReentrancyGuard {
     }
 }
 
-/// @dev Baal contract for guilds
+/// @dev Baal for ETH guilds
 contract Baal is ReentrancyGuard {
+    address public memberExit; // external contract with logic for member exit redemption on removal proposal
     address[] public memberList; // array of member accounts summoned or added by proposal
     uint256 public proposalCount; // counter for proposals submitted
-    uint256 public totalEscrow; // counter for membership proposal tribute ETH escrow
     uint256 public totalSupply; // counter for member votes minted - erc20 compatible
     uint256 public votingPeriod; // period for members to cast votes on proposals in epoch time
     uint8 constant public decimals = 18; // decimals for erc20 vote accounting - 18 is default to match ETH and most erc20
@@ -38,26 +38,27 @@ contract Baal is ReentrancyGuard {
     
     mapping(address => uint256) public balanceOf; // maps member accounts to votes
     mapping(address => bool) public extensions; // maps contracts approved for `memberAction()` 
-    mapping(address => bool) public members; // maps member account to registration
+    mapping(address => Member) public members; // maps member account to registration
     mapping(uint256 => Proposal) public proposals; // maps proposal number to struct details
     
-    event SubmitProposal(address indexed proposer, address indexed target, uint256 indexed proposal, uint256 escrow, uint256 value, bytes data, bool membership, string details); // emits when member submits proposal 
+    event SubmitProposal(address indexed target, uint8 indexed flag, uint256 indexed proposal, uint256 value, bytes data, string details); // emits when member submits proposal 
     event SubmitVote(address indexed member, uint256 balance, uint256 indexed proposal, bool approve); // emits when member submits vote on proposal
     event ProcessProposal(uint256 indexed proposal); // emits when proposal is processed and finalized
     event Transfer(address indexed from, address indexed to, uint256 amount); // emits when member votes are minted or burned - erc20 compatible
     
+    struct Member {
+        bool exists; // tracks member account registration
+        mapping(uint256 => mapping(bool => uint256)) voted; // maps votes on proposals by member account - whether approved and votes cast
+    }
+    
     struct Proposal {
-        address proposer;
         address target; // account that receives low-level call `data` and ETH `value` - if `membership` is `true` and data `length` is 0, account that will receive `value` votes - otherwise, account that will lose votes
-        uint256 escrow;
         uint256 value; // ETH sent from Baal to execute approved proposal low-level call - if `membership` is `true` and data `length` is 0, reflects `votes` to grant member
         uint256 noVotes; // counter for member no votes to calculate approval on processing
         uint256 yesVotes; // counter for member yes votes to calculate approval on processing
         uint256 votingEnds; // termination date for proposal in seconds since epoch - derived from votingPeriod
         bytes data; // raw data sent to `target` account for low-level call
-        bool membership; // flags whether proposal involves adding votes `value` to member account (`target`) - if `false` and no `data`, member votes will be burned
-        bool passed; // flags whether proposal was approved by members
-        bool processed; // flags whether proposal has been processed and executed
+        bool[5] flags; // [governance, membership, removal, passed, processed]
         string details; // context for proposal - could be IPFS hash, plaintext, or JSON
     }
     
@@ -72,38 +73,37 @@ contract Baal is ReentrancyGuard {
         for (uint256 i = 0; i < summoners.length; i++) {
              memberList.push(summoners[i]); // update array of member accounts
              totalSupply += votes[i]; // total votes incremented by summoning with erc20 accounting
-             votingPeriod = _votingPeriod; 
-             name = _name;
-             symbol = _symbol;
              balanceOf[summoners[i]] = votes[i]; // vote weights granted to summoning member with erc20 accounting
              extensions[_extensions[i]] = true; // update mapping of approved extensions
-             members[summoners[i]] = true; // confirm summoning member `exists`
+             members[summoners[i]].exists = true; // confirm summoning member `exists`
              emit Transfer(address(this), summoners[i], votes[i]); // event reflects mint of erc20 votes to summoning members
         }
+        
+        memberExit = _extensions[0]; // set default extension contract for member removal redemptions
+        votingPeriod = _votingPeriod; 
+        name = _name;
+        symbol = _symbol;
     }
     
-    /// @dev Submit proposal to Baal members for approval within voting period - function is `payable`, so can escrow ETH for proposal
+    /// @dev Submit proposal to Baal members for approval within voting period
     /// @param target Account that receives low-level call `data` and ETH `value` - if `membership`, the account that will receive `value` votes - if `removal`, the account that will lose votes
     /// @param value ETH sent from Baal to execute approved proposal low-level call - if `membership`, reflects `votes` to grant member
     /// @param data Raw data sent to `target` account for low-level call 
-    /// @param membership Flags whether proposal involves adding member votes - if `false`, then stage transaction to remove member votes ("ragekick" in moloch parlance)
     /// @param details Context for proposal - could be IPFS hash, plaintext, or JSON
-    function submitProposal(address target, uint256 value, bytes calldata data, bool membership, string calldata details) external nonReentrant payable returns (uint256 count) {
+    function submitProposal(address target, uint8 flag, uint256 value, bytes calldata data, string calldata details) external nonReentrant returns (uint256 count) {
         proposalCount++;
         uint256 proposal = proposalCount;
         
-        if (msg.value > 0) {
-            require(membership, "!membership");
-            totalEscrow += msg.value;
-        }
+        bool[5] memory flags; // [governance, membership, removal, passed, processed]
+        flags[flag] = true; // flag proposal type - must be 
         
-        proposals[proposal] = Proposal(msg.sender, target, msg.value, value, 0, 0, block.timestamp + votingPeriod, data, membership, false, false, details); // push params into proposal struct - start voting timer
+        proposals[proposal] = Proposal(target, value, 0, 0, block.timestamp + votingPeriod, data, flags, details); // push params into proposal struct - start voting timer
         
-        emit SubmitProposal(msg.sender, target, proposal, msg.value, value, data, membership, details);
+        emit SubmitProposal(target, flag, proposal, value, data, details);
         return proposal;
     }
     
-    /// @dev Submit vote - caller must have uncast votes, proposal must exist and voting period must not have ended
+    /// @dev Submit vote - proposal must exist and voting period must not have ended
     /// @param proposal Number of proposal in `proposals` mapping to cast vote on 
     /// @param approve If `true`, member will cast `yesVotes` onto proposal - if `false, `noVotes` will be counted
     function submitVote(uint256 proposal, bool approve) external nonReentrant {
@@ -111,8 +111,10 @@ contract Baal is ReentrancyGuard {
         require(proposal <= proposalCount, "!exist");
         require(proposals[proposal].votingEnds >= block.timestamp, "ended");
         
-        if (approve) {proposals[proposal].yesVotes += balance;} // cast 'yes' votes per member balance
-        else {proposals[proposal].noVotes += balance;} // cast 'no' votes per member balance
+        if (approve) {proposals[proposal].yesVotes += balance;} // cast 'yes' votes per member balance to proposal
+        else {proposals[proposal].noVotes += balance;} // cast 'no' votes per member balance to proposal
+        
+        members[msg.sender].voted[proposal][approve] = balance; // record member vote to account
         
         emit SubmitVote(msg.sender, balance, proposal, approve);
     }
@@ -122,46 +124,38 @@ contract Baal is ReentrancyGuard {
     function processProposal(uint256 proposal) external nonReentrant returns (bool success, bytes memory retData) {
         require(proposal <= proposalCount, "!exist");
         require(proposals[proposal].votingEnds <= block.timestamp, "!ended");
-        require(!proposals[proposal].processed, "processed");
+        require(!proposals[proposal].flags[4], "processed");
       
         address target = proposals[proposal].target;
-        bool membership = proposals[proposal].membership;
+        uint256 value = proposals[proposal].value;
         
         if (proposals[proposal].yesVotes > proposals[proposal].noVotes) { // check if proposal approved by simple majority of members
-            proposals[proposal].passed = true;
-            if (membership) { // check into membership proposal
-                uint256 value = proposals[proposal].value;
+            proposals[proposal].flags[2] = true; // flag that vote passed
+            if (proposals[proposal].flags[0]) { // check into governance proposal
+                extensions[target] = true;
+                votingPeriod = value;
+            } else if (proposals[proposal].flags[1]) { // check into membership proposal
                 // update list of member accounts if new
-                if (!members[msg.sender]) { 
+                if (!members[msg.sender].exists) { 
                     memberList.push(target);
                 } 
-                
                 totalSupply += value; // add to total member votes
                 balanceOf[target] += value; // add to `target` member votes
-                
                 emit Transfer(address(this), target, value); // event reflects mint of erc20 votes
-            } else if (!membership && proposals[proposal].data.length == 0) { // alternatively, check into removal proposal
+            } else if (proposals[proposal].flags[2]) { // alternatively, check into removal proposal
                 uint256 balance = balanceOf[target];
-                
-                totalSupply -= balance; // subtract from total member votes
+                MemberAction(memberExit).memberBurn(target, balance); // burn `balance` againt `memberExit` based on `target` member 
+                totalSupply -= balance; // subtract `balance` from total member votes
                 balanceOf[target] = 0; // reset member votes
-                members[target] = false; // reset member registration status
-                
+                members[target].exists = false; // reset member registration status
                 emit Transfer(address(this), address(0), balance); // event reflects burn of erc20 votes
-            } else if (address(this).balance - totalEscrow >= proposals[proposal].value)  { // otherwise, check into low-level call if sufficient ETH balance against membership escrows
+            } else { // otherwise, check into low-level call
                 (bool callSuccess, bytes memory returnData) = target.call{value: proposals[proposal].value}(proposals[proposal].data); // execute low-level call
                 return (callSuccess, returnData); // return call success and data
             }
-        } else { // if proposal did not pass, return any escrowed ETH to `proposer`
-            if (proposals[proposal].escrow > 0) {
-                uint256 escrow = proposals[proposal].escrow;
-                (bool callSuccess, ) = proposals[proposal].proposer.call{value: escrow}("");
-                require(callSuccess, "!ethCall");
-                totalEscrow -= escrow;
-            }
         }
         
-        proposals[proposal].processed = true; // record that proposal processed
+        proposals[proposal].flags[4] = true; // flag that proposal processed
         emit ProcessProposal(proposal);
     }
     
@@ -169,17 +163,17 @@ contract Baal is ReentrancyGuard {
     /// @param target Account to call to trigger member action - must be approved in `extensions`
     /// @param amount Number of member votes to submit in action
     /// @param mint Confirm whether transaction involves mint - if `false,` perform balance-based burn
-    function memberAction(address target, uint256 amount, bool mint) external nonReentrant {
-        require(members[msg.sender], "!member");
+    function memberAction(address target, uint256 amount, bool mint) external payable nonReentrant {
+        require(members[msg.sender].exists, "!member");
         require(extensions[target], "!extension"); 
         
         if (mint) {
-            MemberAction(target).memberMint(msg.sender, amount);
-            totalSupply += amount; // add to total member votes
-            balanceOf[msg.sender] += amount; // add member votes
-            emit Transfer(address(this), msg.sender, amount); // event reflects mint of erc20 votes
+            uint256 minted = MemberAction(target).memberMint{value: msg.value}(msg.sender); // mint from `target` `msg.value` return based on member
+            totalSupply += minted; // add to total member votes
+            balanceOf[msg.sender] += minted; // add member votes
+            emit Transfer(address(this), msg.sender, minted); // event reflects mint of erc20 votes
         } else {    
-            MemberAction(target).memberBurn(msg.sender, amount);
+            MemberAction(target).memberBurn(msg.sender, amount); // burn `amount` againt `target` based on member
             totalSupply -= amount; // subtract from total member votes
             balanceOf[msg.sender] -= amount; // subtract member votes
             emit Transfer(address(this), address(0), amount); // event reflects burn of erc20 votes
