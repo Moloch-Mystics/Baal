4a951ee214f8cf1bb41ec0ecfd13accdaa00b21e
mkdir <projectdevelopinginbox@gmail.com>
2021-06-24 14:15:58 -0600

    tab fix vim / github

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,13 +5,13 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: {
-		compilers: [
-			{
-				version: "0.8.0"
-			},
-			{
-				version: "0.8.6"
-			}
-		]
-	},
+    compilers: [
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.8.6"
+      }
+    ]
+  },
 };
