b04ebb98eb42b78a4559c8056b675924d6b47c36
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-08-22 19:20:46 -0400

    Create TestERC20.sol

---
+++ contracts/mock/TestERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 supply) ERC20(name, symbol) {
+        _mint(msg.sender, supply);
+    }
+}
