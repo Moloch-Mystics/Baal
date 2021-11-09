3a13f2e00f005fa7b1b44ea8730cbe0368874f9b
Isaac Patka <isaacpatka@Isaacs-MacBook-Pro-2.local>
2021-11-09 12:30:29 -0500

    remove vestigial multicall

--- contracts/Baal.sol
+++ contracts/Baal.sol
@@ -1012,28 +1012,6 @@ contract Baal is Executor, Initializable {
     /***************
     HELPER FUNCTIONS
     ***************/
-    /// @notice Allows batched calls to Baal.
-    /// @param data An array of payloads for each call.
-    function multicall(bytes[] calldata data)
-        external
-        returns (bytes[] memory results)
-    {
-        results = new bytes[](data.length);
-        unchecked {
-            for (uint256 i = 0; i < data.length; i++) {
-                (bool success, bytes memory result) = address(this)
-                    .delegatecall(data[i]);
-                if (!success) {
-                    if (result.length < 68) revert();
-                    assembly {
-                        result := add(result, 0x04)
-                    }
-                    revert(abi.decode(result, (string)));
-                }
-                results[i] = result;
-            }
-        }
-    }
 
     /// @notice Returns confirmation for 'safe' ERC-721 (NFT) transfers to Baal.
     function onERC721Received(
