95cd1b185c0b7cf1de862ce21a0b1b166a1e6d2c
dekanbro <dekanbrown@odyssy.io>
2022-03-22 09:44:06 -0600

    add a event to the tribute tool contract

--- contracts/tools/TributeEscrow.sol
+++ contracts/tools/TributeEscrow.sol
@@ -2,13 +2,14 @@
 pragma solidity >=0.8.0;
 import "../Baal.sol";
 
-import "hardhat/console.sol";
+//  import "hardhat/console.sol";
 
 interface IERC20 {
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
 contract TributeEscrow {
+    event TributeProposal(address indexed baal, address token, uint256 amount, address recipient, uint256 proposalId);
     struct Escrow {
         address token;
         address applicant;
@@ -118,22 +119,23 @@ contract TributeEscrow {
             baal.target()
         );
         baal.submitProposal(encodedProposal, expiration, details);
+        emit TributeProposal(address(baal), token, amount, recipient, proposalId);
     }
 
     function releaseEscrow(uint32 proposalId) external {
-        console.log("releasing");
+        // console.log("releasing");
         Baal baal = Baal(msg.sender);
         Escrow storage escrow = escrows[address(baal)][proposalId];
         require(!escrow.released, "Already released");
-        console.log("releasing1b");
+        // console.log("releasing1b");
 
         bool[4] memory status = baal.getProposalStatus(proposalId);
-        console.log("releasing1c");
+        // console.log("releasing1c");
         require(status[2], "Not passed");
         escrow.released = true;
 
         IERC20 token = IERC20(escrow.token);
-        console.log("releasing2");
+        // console.log("releasing2");
 
         require(
             token.transferFrom(escrow.applicant, escrow.safe, escrow.amount),
