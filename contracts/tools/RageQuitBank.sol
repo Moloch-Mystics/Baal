954feb6d3b0ca229ca7ccd31e655bef8d432e3ff
Brian Rossetti <brossetti1@gmail.com>
2022-06-13 18:35:35 -0400

    RageQuitBank.lootRate (contracts/tools/RageQuitBank.sol#6) should be constant RageQuitBank.shareRate (contracts/tools/RageQuitBank.sol#7) should be constant
    Reference: https://github.com/crytic/slither/wiki/Detector-Documentation#state-variables-that-could-be-declared-constant
    
    self explainatory
    
    https://docs.soliditylang.org/en/v0.5.3/style-guide.html#constants

--- contracts/tools/RageQuitBank.sol
+++ contracts/tools/RageQuitBank.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.8.0;
 
 contract RageQuitBank {
     address public baal;
-    uint96 lootRate = 2;
-    uint96 shareRate = 2;
+    uint96 constant LOOT_RATE = 2;
+    uint96 constant SHARE_RATE = 2;
 
     function init(address _baal) external {
         require(_baal != address(0), "transfer from the zero address");
@@ -12,8 +12,8 @@ contract RageQuitBank {
     }
     
     function memberAction(address, uint96 loot, uint96 shares) external payable returns (uint96 lootOut, uint96 sharesOut) {
-        require(msg.sender == baal,'!baal');
-        lootOut = loot * lootRate;
-        sharesOut = shares * shareRate;
+        require(msg.sender == baal, "!baal");
+        lootOut = loot * LOOT_RATE;
+        sharesOut = shares * SHARE_RATE;
     }
 }
