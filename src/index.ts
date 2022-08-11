b6d06ec8cd5f13d7ba41715f6e1c71bf99151549
Brian Rossetti <brossetti1@gmail.com>
2022-08-11 10:02:50 -0600

    adding additional docs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 /*
   in order to adjust the build folder:
-    1) add any files here you want in the build
-    2) copy the file path
-    3) add the path to the ts.config.build.json under the **include: [...]** configuration.
+    1) import any files here you want in the build.
+    2) copy the file path of the import.
+    3) add the path to the ts.config.build.json under the { include: [...] } configuration.
+    4) bump package.json version to publish a new package to npm.
 */
 
 export { Baal } from './types/contracts/Baal.sol/Baal';
