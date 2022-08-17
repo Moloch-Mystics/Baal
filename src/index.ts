a625ad845fbbd70a1ab817bada2ee31dee0c9273
Brian Rossetti <brossetti1@gmail.com>
2022-08-17 16:33:23 -0600

    adding new build config

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@
     4) bump package.json version to publish a new package to npm.
 */
 
-export { Baal } from './types/contracts/Baal.sol/Baal';
-export { Baal__factory as BaalFactory } from './types/factories/contracts/Baal.sol/Baal__factory';
-export { BaalSummoner } from './types/contracts/Baal.sol/BaalSummoner';
-export { BaalSummoner__factory as BaalSummonerFactory } from './types/factories/contracts/Baal.sol/BaalSummoner__factory';
+export { Baal } from './types/contracts/Baal';
+export { Baal__factory as BaalFactory } from './types/factories/contracts/Baal__factory';
+export { BaalSummoner } from './types/contracts/BaalSummoner';
+export { BaalSummoner__factory as BaalSummonerFactory } from './types/factories/contracts/BaalSummoner__factory';
 export { Loot } from './types/contracts/LootERC20.sol/Loot';
 export { Loot__factory as LootFactory } from './types/factories/contracts/LootERC20.sol/Loot__factory';
 export { Shares } from './types/contracts/SharesERC20.sol/Shares';
 export { Shares__factory as SharesFactory } from './types/factories/contracts/SharesERC20.sol/Shares__factory';
-export { Poster } from './types/contracts/tools/Poster';
-export { Poster__factory as PosterFactory } from './types/factories/contracts/tools/Poster__factory';
+export { Poster } from './types/contracts/utils/Poster';
+export { Poster__factory as PosterFactory } from './types/factories/contracts/utils/Poster__factory';
 export { TributeMinion } from './types/contracts/tools/TributeMinion.sol/TributeMinion';
 export { TributeMinion__factory as TributeMinionFactory } from './types/factories/contracts/tools/TributeMinion.sol/TributeMinion__factory';
 export { MultiSend } from './types/@gnosis.pm/safe-contracts/contracts/libraries/MultiSend';
 export { MultiSend__factory as MultiSendFactory } from './types/factories/@gnosis.pm/safe-contracts/contracts/libraries/MultiSend__factory';
 
 // Interfaces
-export { IBaalToken } from './types/contracts/Baal.sol/IBaalToken';
+export { IBaalToken } from './types/contracts/interfaces/IBaalToken';
 export { IBaal } from './types/contracts/interfaces/IBaal';
